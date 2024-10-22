0f653f142dedb121fa8117abf1246c17f926fa80
Martin Contreras U <martin.contreras.u@outlook.com>
2024-10-22 16:20:51 -0300

    Add files via upload

--- aws_snippets.py
+++ aws_snippets.py
@@ -1,40 +1,58 @@
-import psycopg2 
 import boto3
-import sys
-sys.path.append("../src")
-import urllib.parse
-
+from urllib.parse import quote_plus
 from sqlalchemy import create_engine
+import psycopg2
+from sqlalchemy.engine.url import URL
+import redshift_connector
 
 rs_connection = None
 ssm = boto3.client('ssm', region_name='us-east-1')
 
 
-def load_cnx_params():
+def _load_cnx_params(username: str, user_role: str):
     """
     El usuario y contrasena
     se obtienen de Parameter Store
     :return: Diccionario con parametros de conexion
     """
     cnx_params = {
-        "username":ssm.get_parameter(Name="/sagemaker/redshift/mcontreras/user_ds_1")['Parameter']['Value'],
-        "password":ssm.get_parameter(Name="/sagemaker/redshift/mcontreras/pass_ds_1", WithDecryption=True)['Parameter']['Value']
+        "username":ssm.get_parameter(Name=f"/sagemaker/redshift/{username}/user_{user_role}")['Parameter']['Value'],
+        "password":ssm.get_parameter(Name=f"/sagemaker/redshift/{username}/pass_{user_role}", WithDecryption=True)['Parameter']['Value']
                                       }
+
     if all(cnx_params.values()):
         return cnx_params
     else:
         raise Exception("Parametros de conexion a Redshift faltantes!")
 
 
-def get_engine():
-    """Returns the redshift engine for pandas queries"""
+def get_engine(username: str, user_role: str, return_uri=False):
+    """
+    Returns the redshift engine for pandas queries
+    """
     # getting credentials
-    username, password = load_cnx_params().values()
-    #password = urllib.parse.quote_plus(password)
-    host = "redshift-privado-prod.c0iwf7ndlvaw.us-east-1.redshift.amazonaws.com"
-    dbname = "prod"
-    port=5439
-    
+    username_parsed, password = _load_cnx_params(username, user_role).values()
+
+    uri = f'postgresql://{username_parsed}:{quote_plus(password)}@redshift-privado-prod.c0iwf7ndlvaw.us-east-1.redshift.amazonaws.com:5439/prod'
+
+    if return_uri: 
+        return uri
+        
     # Creating the engine
-    connection = f'postgres://{username}:{password}@redshift-privado-prod.c0iwf7ndlvaw.us-east-1.redshift.amazonaws.com:5439/prod'
-    return create_engine(connection)
+    eng = create_engine(uri)
+    
+    return eng
+
+def get_redshift_engine(username: str, user_role: str, return_uri=False):
+
+    username_parsed, password = _load_cnx_params(username, user_role).values()
+    # build the sqlalchemy URL
+    conn = redshift_connector.connect(
+    host='redshift-privado-prod.c0iwf7ndlvaw.us-east-1.redshift.amazonaws.com', # Amazon Redshift host
+    port=5439, # Amazon Redshift port
+    database='prod', # Amazon Redshift database
+    user=username_parsed, # Amazon Redshift username
+    password=password # Amazon Redshift password
+    )
+    
+    return conn
